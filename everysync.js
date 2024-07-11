250756e5bb7af5c1e274dda5d672957c41c065f0
Matteo Collina <hello@matteocollina.com>
2024-07-11 18:33:23 +0200

    implemented makeSync and wire
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- everysync.js
+++ everysync.js
@@ -10,6 +10,61 @@ const {
   TO_WORKER
 } = require('./lib/indexes')
 
+function makeSync (data) {
+  const metaView = new Int32Array(data)
+
+  const res = Atomics.wait(metaView, TO_WORKER, 0, 1000)
+  Atomics.store(metaView, TO_WORKER, 0)
+
+  if (res === 'ok') {
+    const obj = read(data, OFFSET)
+
+    const api = {}
+    for (const key of obj) {
+      api[key] = (...args) => {
+        write(data, { key, args }, OFFSET)
+        Atomics.store(metaView, TO_MAIN, 1)
+        Atomics.notify(metaView, TO_MAIN, 1)
+        const res = Atomics.wait(metaView, TO_WORKER, 0)
+        Atomics.store(metaView, TO_WORKER, 0)
+        if (res === 'ok') {
+          const obj = read(data, OFFSET)
+          return obj
+        } else {
+          throw new Error('Worker failed to respond')
+        }
+      }
+    }
+
+    return api
+  } else {
+    throw new Error('Worker failed to initialize')
+  }
+}
+
+async function wire (data, obj) {
+  write(data, Object.keys(obj), OFFSET)
+
+  const metaView = new Int32Array(data)
+
+  Atomics.store(metaView, TO_WORKER, 1)
+  Atomics.notify(metaView, TO_WORKER)
+
+  while (true) {
+    const res = Atomics.wait(metaView, TO_MAIN, 0)
+    Atomics.store(metaView, TO_MAIN, 0)
+
+    if (res === 'ok') {
+      const { key, args } = read(data, OFFSET)
+      // This is where the magic happens
+      const result = await obj[key](...args)
+      write(data, result, OFFSET)
+      Atomics.store(metaView, TO_WORKER, 1)
+      Atomics.notify(metaView, TO_WORKER, 1)
+    }
+  }
+}
+
 class EverySyncWorker extends EventEmitter {
   constructor({ module, maxByteLength }) { 
     super()
@@ -17,8 +72,6 @@ class EverySyncWorker extends EventEmitter {
     this._data = new SharedArrayBuffer(1024, {
       maxByteLength: maxByteLength || 64 * 1024 * 1024, // 64MB
     })
-    // Indexes:
-    this._metaView = new Int32Array(this._data)
     this.worker = new Worker(join(__dirname, 'lib', 'worker.js'), {
       workerData: {
         module,
@@ -30,36 +83,7 @@ class EverySyncWorker extends EventEmitter {
       this.emit('error', err)
     })
 
-    const res = Atomics.wait(this._metaView, TO_WORKER, 0, 1000)
-    Atomics.store(this._metaView, TO_WORKER, 0)
-
-    if (res === 'ok') {
-      const obj = read(this._data, OFFSET)
-      this.api = this._buildApi(obj)
-    } else {
-      throw new Error('Worker failed to initialize')
-    }
-  }
-
-  _buildApi (obj) {
-    const api = {}
-    for (const key of obj) {
-      api[key] = (...args) => {
-        write(this._data, { key, args }, OFFSET)
-        Atomics.store(this._metaView, TO_MAIN, 1)
-        Atomics.notify(this._metaView, TO_MAIN, 1)
-        const res = Atomics.wait(this._metaView, TO_WORKER, 0)
-        Atomics.store(this._metaView, TO_WORKER, 0)
-        if (res === 'ok') {
-          const obj = read(this._data, OFFSET)
-          return obj
-        } else {
-          throw new Error('Worker failed to respond')
-        }
-      }
-    }
-
-    return api
+    this.api = makeSync(this._data)
   }
 
   terminate () {
@@ -67,4 +91,6 @@ class EverySyncWorker extends EventEmitter {
   }
 }
 
+module.exports.makeSync = makeSync
+module.exports.wire = wire
 module.exports.EverySyncWorker = EverySyncWorker
--- test/base.test.js
+++ test/base.test.js
@@ -3,7 +3,8 @@
 const { test } = require('node:test')
 const { join } = require('node:path')
 const assert = require('node:assert/strict')
-const { EverySyncWorker } = require('..')
+const { Worker } = require('node:worker_threads')
+const { EverySyncWorker, makeSync } = require('..')
 
 test('make a async function sync', async (t) => {
   const everysync = new EverySyncWorker({
@@ -15,4 +16,23 @@ test('make a async function sync', async (t) => {
   })
 
   assert.strictEqual(everysync.api.foo(), 'foo')
+})
+
+test('makeSync and wire', async (t) => {
+  const buffer = new SharedArrayBuffer(1024, {
+    maxByteLength: 64 * 1024 * 1024,
+  })
+  const worker = new Worker(join(__dirname, 'fixtures', 'echo.mjs'), {
+    workerData: {
+      data: buffer,
+    },
+  })
+
+  const api = makeSync(buffer)
+
+  t.after(() => {
+    worker.terminate()
+  })
+
+  assert.strictEqual(api.echo(42), 42)
 })
